510abf144961403bc245b99e283e265ba2aaf2ec
heysarahshere <heysarahshere@gmail.com>
2020-09-16 16:42:33 -0700

    Added styling, BuildControl and Burger components

--- burger-builder/src/components/Layout/Layout.js
+++ burger-builder/src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 // import Aux from '../../hoc/Auxiliary';
+import classes from './Layout.module.css';
 
 const layout = (props) => (
     <React.Fragment>
     <div>Toolbar, Sidedrawer, Backdrop</div>
-    <main>
+    <main className={classes.Content}>
         { props.children}
     </main>
     </React.Fragment>
--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
-
+import Burger from '../../components/Burger/Burger';
+import BuildControls from'../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
+    // constructor(props){
+    //     super(props);
+    //     this.state = {}
+    // }
+    state = {
+        ingredients: {
+            lettuce: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        }
+    }
+
     render() {
         return (
             <React.Fragment>
-                <div>Burger</div>
-                <div>Build Controls</div>
+                <Burger ingredients={this.state.ingredients}/>
+                <BuildControls/>
             </React.Fragment>
         );
     }
