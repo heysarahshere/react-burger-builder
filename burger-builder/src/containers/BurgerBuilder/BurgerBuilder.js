0731e93c4c7db2f2da6ab6d2d1225201d0d603e9
heysarahshere <heysarahshere@gmail.com>
2020-09-16 20:48:56 -0700

    Added modal functionality

--- burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from'../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
     lettuce: 0.15,
@@ -22,7 +23,12 @@ class BurgerBuilder extends Component {
             meat: 0
         },
         totalPrice: 4.00,
-        purchasable: false
+        purchasable: false,
+        purchasing: false
+    }
+
+    purchaseHandler = () => {
+        this.setState({purchasing: true});
     }
     
     updatePurchaseState (ingredients) {
@@ -77,14 +83,17 @@ class BurgerBuilder extends Component {
         }
         return (
             <React.Fragment>
-                <Modal />
+                <Modal show={this.state.purchasing}>
+                    <OrderSummary ingredients={this.state.ingredients}/>
+                </Modal>
                 <Burger ingredients={this.state.ingredients}/>
                 <BuildControls
                     ingredientAdded={this.addIngredientHandler}
                     ingredientRemoved={this.removeIngredientHandler}
                     disabled={disabledInfo}
                     price={this.state.totalPrice}
-                    purchasable={this.state.purchasable}/>
+                    purchasable={this.state.purchasable}
+                    ordered={this.purchaseHandler}/>
             </React.Fragment>
         );
     }
