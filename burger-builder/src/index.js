6db4b4e474b3677fb117714e9c241aa04719a0c5
heysarahshere <heysarahshere@gmail.com>
2020-10-03 00:31:37 -0700

    Update index.js

--- burger-builder/src/index.js
+++ burger-builder/src/index.js
@@ -2,13 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import reducer from './store/reducer';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
+// import reducer from './store/reducer';
 
-const store = createStore(reducer);
+import registerServiceWorker from './registerServiceWorker';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// const store = createStore(reducer);
+const store = createStore(burgerBuilderReducer, composeEnhancers(
+  applyMiddleware(thunk)
+));
+
 
 const app = (
   <Provider store={store}>
@@ -20,8 +29,11 @@ const app = (
 );
 ReactDOM.render( app, document.getElementById('root')
 );
+registerServiceWorker();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
+
+
