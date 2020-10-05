1d851eb44ae1915b006054b21c7966bee1ec9649
heysarahshere <heysarahshere@gmail.com>
2020-10-04 17:53:24 -0700

    Adding auth

--- burger-builder/src/containers/Auth/Auth.js
+++ burger-builder/src/containers/Auth/Auth.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
+import classes from './Auth.module.css';
 
 
 class Auth extends Component {
@@ -38,12 +39,57 @@ class Auth extends Component {
         }
     }
 
+    checkValidity(value, rules) {
+        let isValid = true;
+        if (!rules) {
+            return true;
+        }
+        
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid
+        }
+
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        return isValid;
+    }
+
+    inputChangedHandler= (event, controlName) => {
+        const updatedControls = {
+            ...this.state.controls,
+            [controlName]: {
+                ...this.state.controls[controlName],
+                value: event.target.value,
+                valid: this.checkValidity(event.target.value, 
+                    this.state.controls[controlName].validation),
+                    touched: true
+            }
+        };
+        this.setState({controls: updatedControls});
+    }
+
     render () {
         const formElementsArray = [];
-        for (let key in this.state.orderForm) {
+        for (let key in this.state.controls) {
             formElementsArray.push({
                 id: key,
-                config: this.state.orderForm[key]
+                config: this.state.controls[key]
             });
         }
 
@@ -60,7 +106,7 @@ class Auth extends Component {
             />
         ));
         return (
-            <div>
+            <div className={classes.Auth}>
                 <form>
                     {form}
                     <Button btnType="Success">Submit</Button>
